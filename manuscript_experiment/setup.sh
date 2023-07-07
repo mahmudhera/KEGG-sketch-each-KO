743506b9cb867bdd8ba9f1c1b35bebf58324a63c
Mahmudur <mahmudhera93@gmail.com>
2023-07-07 15:51:59 -0400

    revised to do less work

--- manuscript_experiment/setup.sh
+++ manuscript_experiment/setup.sh
@@ -27,16 +27,16 @@ echo "$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseFull $numGen
 $scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseFull $numGenomesFullDB
 
 # create the truncated genome reference database
-echo "$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseTruncated $numGenomesTruncatedDB"
-$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseTruncated $numGenomesTruncatedDB
+#echo "$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseTruncated $numGenomesTruncatedDB"
+#$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseTruncated $numGenomesTruncatedDB
 
 # create the mapping files required for the protein database
-echo "$scriptDir/make_mapping_file.py "$genomePath""
+#echo "$scriptDir/make_mapping_file.py "$genomePath""
 #$scriptDir/make_mapping_file.py "$genomePath"
 # no need because all genomes have mapping files ready
 
 # create the protein reference database
 echo "$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseFull $numGenomesFullDB"
 $scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseFull $numGenomesFullDB
-echo "$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseTruncated $numGenomesTruncatedDB"
-$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseTruncated $numGenomesTruncatedDB
+#echo "$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseTruncated $numGenomesTruncatedDB"
+#$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseTruncated $numGenomesTruncatedDB
